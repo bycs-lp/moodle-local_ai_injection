2cafec5331016c922d8c3a57d84d5ef832bd4826
PhMemmel <65113153+PhMemmel@users.noreply.github.com>
2026-03-17 10:56:21 +0100

    Bump version in aiinjection_alttext

--- injection/alttext/version.php
+++ injection/alttext/version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'aiinjection_alttext';
-$plugin->version = 2026020900;
+$plugin->version = 2026031700;
 $plugin->requires = 2025041400; // Moodle 5.0+.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '0.1';
+$plugin->release = '0.2';
